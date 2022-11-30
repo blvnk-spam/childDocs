1b1314758d0106e0eb1259c5ec8553eeeb7401bf
Ben Foster <93410424+blvnk-spam@users.noreply.github.com>
2022-11-29 23:10:15 -0700

    Add files via upload

---
+++ childDocs/Website/adminLogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta name = "viewport" content = "width= devive-width, initial-scale=1.0">
+        <title>Home Page</title>
+        <style>
+            .flex-parent{
+                display: flex;
+            }
+
+            .jc-center {
+                justify-content: center; 
+            }
+
+            .button {
+
+            }
+        </style>
+    
+    </head>
+
+    <h1>childDocs</h1>
+    <body bgcolor="FBB917">
+        <div><h2>Succesfully selected admin as user type</h2></div>
+    </body>
+</html>
